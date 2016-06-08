c8a08e24696de8f58f27eb5d7bc73b06641a031b
kevinkohlstedt <kevinkohlstedt@users.noreply.github.com>
2016-06-08 09:37:47 -0500

    cookin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
+# root 'beers#index'  ADD THIS BACK IN ONCE BEERS PAGE IS LIVE
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
