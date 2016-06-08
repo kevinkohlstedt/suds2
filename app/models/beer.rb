ebbfe6b55fef03e048016ea6bb74e7be02ae3522
kevinkohlstedt <kevinkohlstedt@users.noreply.github.com>
2016-06-08 12:15:31 -0500

    Validations & Formatting

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -8,6 +8,7 @@ validates :style, :presence => true
 validates :rating_1_10, :presence => true
 validates :rating_1_10, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 10}
 validates :abv, :presence => true
+validates :abv, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100}
 validates :location_consumed, :presence => true
 validates :tasting_notes, :presence => true
 
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,8 @@
 class Brewery < ActiveRecord::Base
 has_many :beers
+
+validates :brewery_name, :presence => true, :uniqueness => true
+validates :brewery_location, :presence => true
+validates :image_url, :presence => true
+
 end
