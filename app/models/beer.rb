8cc4409c839590650959ebe9a1c73f3edb76d89d
kevinkohlstedt <kevinkohlstedt@users.noreply.github.com>
2016-06-08 11:48:20 -0500

    updates

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,14 @@
 class Beer < ActiveRecord::Base
 belongs_to :users , :class_name => "User", :foreign_key => "users_id"
 belongs_to :brewery
+
+validates :beer_name, :presence => true, :uniqueness => true
+validates :brewery_name, :presence => true
+validates :style, :presence => true
+validates :rating_1_10, :presence => true
+validates :rating_1_10, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 10}
+validates :abv, :presence => true
+validates :location_consumed, :presence => true
+validates :tasting_notes, :presence => true
+
 end
