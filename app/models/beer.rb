b246dfffad414f2a11baea3313d98cda4b328dbd
kevinkohlstedt <kevinkohlstedt@users.noreply.github.com>
2016-06-08 10:17:44 -0500

    rollin'

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,2 +1,4 @@
 class Beer < ActiveRecord::Base
+belongs_to :users , :class_name => "User", :foreign_key => "users_id"
+belongs_to :brewery
 end
--- app/models/brewery_to_beer.rb
+++ app/models/brewery_to_beer.rb
@@ -1,2 +1,3 @@
 class BreweryToBeer < ActiveRecord::Base
+has_many :beers
 end
